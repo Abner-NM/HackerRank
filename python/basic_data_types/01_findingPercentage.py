c67441f6898f53ac5ef9f26a52d847c0d1749404
Abner-NM <abnernyangena@gmail.com>
2023-04-17 06:50:06 +0300

    HackerRank data types

--- python/basic_data_types/01_findingPercentage.py
+++ python/basic_data_types/01_findingPercentage.py
@@ -1,9 +1,14 @@
-# Pseudocode
 if __name__ == '__main__':
 	n = int(input())
 	student_marks = {}
-	for _ in range(n):
+	for i in range(n):
 		name, *line = input().split()
 		scores = list(map(float, line))
 		student_marks[name] = scores
 	query_name = input()
+	marks = student_marks[query_name]
+	total = sum(marks)
+	avg = total / len(marks)
+	r_avg = round(avg, 2)
+	formatted_avg = "{:.2f}".format(r_avg)
+	print(formatted_avg)
